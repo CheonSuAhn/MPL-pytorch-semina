43011df37d4f5c2601ccf1a8c3a6282d41c08619
CheonSuAhn <93701068+CheonSuAhn@users.noreply.github.com>
2021-12-08 22:51:20 +0900

    Update data.py

--- data.py
+++ data.py
@@ -17,8 +17,9 @@ cifar100_std = (0.2675, 0.2565, 0.2761)
 normal_mean = (0.5, 0.5, 0.5)
 normal_std = (0.5, 0.5, 0.5)
 
-
+# only comment about cifar10 dataset
 def get_cifar10(args):
+    # setting transform for labeled data (weak augmentation)
     transform_labeled = transforms.Compose([
         transforms.RandomHorizontalFlip(),
         transforms.RandomCrop(size=args.resize,
@@ -27,29 +28,34 @@ def get_cifar10(args):
         transforms.ToTensor(),
         transforms.Normalize(mean=cifar10_mean, std=cifar10_std)
     ])
+    # setting transform for validation(test) data (no augmentation)
     transform_val = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize(mean=cifar10_mean, std=cifar10_std)
     ])
+    # setting the base dataset to CIFAR10
     base_dataset = datasets.CIFAR10(args.data_path, train=True, download=True)
-
+       
+    # setting index to split train datset into labeled and unlabeled dataset
     train_labeled_idxs, train_unlabeled_idxs = x_u_split(args, base_dataset.targets)
-    # train_labeled_idxs, train_unlabeled_idxs = x_u_split_test(args, base_dataset.targets)
-
+    
+    # setting train labeled dataset using transform for labeled data
     train_labeled_dataset = CIFAR10SSL(
         args.data_path, train_labeled_idxs, train=True,
         transform=transform_labeled
     )
-
+    
+    # setting train unlabeled dataset using TransformMPL(transform for unlabeled data)
     train_unlabeled_dataset = CIFAR10SSL(
         args.data_path, train_unlabeled_idxs,
         train=True,
         transform=TransformMPL(args, mean=cifar10_mean, std=cifar10_std)
     )
-
+    
+    # setting test dataset using transform for validation(test) data
     test_dataset = datasets.CIFAR10(args.data_path, train=False,
                                     transform=transform_val, download=False)
-
+    # return the test dataset and training dataset composed of labeled and unlabeled dataset
     return train_labeled_dataset, train_unlabeled_dataset, test_dataset
 
 
@@ -86,7 +92,7 @@ def get_cifar100(args):
 
     return train_labeled_dataset, train_unlabeled_dataset, test_dataset
 
-
+# split train datset into labeled and unlabeled dataset according to the pre-defined labeled data number(args.num_labeled)
 def x_u_split(args, labels):
     label_per_class = args.num_labeled // args.num_classes
     labels = np.array(labels)
@@ -106,6 +112,7 @@ def x_u_split(args, labels):
         labeled_idx = np.hstack([labeled_idx for _ in range(num_expand_x)])
     np.random.shuffle(labeled_idx)
     return labeled_idx, unlabeled_idx
+    # number of labeled dataset is pre-defined and number of unlabeled datset is same as all training dataset
 
 
 def x_u_split_test(args, labels):
@@ -131,33 +138,38 @@ def x_u_split_test(args, labels):
     np.random.shuffle(unlabeled_idx)
     return labeled_idx, unlabeled_idx
 
-
+# transform for unlabeled data
 class TransformMPL(object):
     def __init__(self, args, mean, std):
+        # setting the hyperparameter for random augmentation
         if args.randaug:
             n, m = args.randaug
         else:
             n, m = 2, 10  # default
-
+        
+        # original data transformed with weak augmentation
         self.ori = transforms.Compose([
             transforms.RandomHorizontalFlip(),
             transforms.RandomCrop(size=args.resize,
                                   padding=int(args.resize*0.125),
                                   padding_mode='reflect')])
+        # augmented data transformed with strong augmentation (random augmentation)
         self.aug = transforms.Compose([
             transforms.RandomHorizontalFlip(),
             transforms.RandomCrop(size=args.resize,
                                   padding=int(args.resize*0.125),
                                   padding_mode='reflect'),
             RandAugment(n=n, m=m)])
+        
+        # normalization according to the dataset
         self.normalize = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize(mean=mean, std=std)])
 
     def __call__(self, x):
         ori = self.ori(x)
-        aug = self.aug(x)
-        return self.normalize(ori), self.normalize(aug)
+        aug = self.aug(x)   # transform using strong augmentation (random augmentation)
+        return self.normalize(ori), self.normalize(aug) # output two data(batchsize * 2) (weak augmentation and strong augmentation)
 
 
 class CIFAR10SSL(datasets.CIFAR10):
